da4bac1c5dd8d7be1e49c3e2fc9772c5778c519b
Hugo Milnehome <40179907+suprattle@users.noreply.github.com>
2020-04-08 23:57:37 -0400

    added post monitoring
    added post monitoring

--- instagram.py
+++ instagram.py
@@ -6,20 +6,20 @@ from bs4 import BeautifulSoup
 
 start = input("Input Anything To Start: ")
 instagramUsername = input("Instagran Username: ")
-if "@" in instargarmUsername:
+if "@" in instagramUsername:
     instagramUsername = instagramUsername.replace("@", "")
 retryDelay = input("Monitor Delay: ")
 print("Monitor Started")
-discordWebhook = "" #put your webhook in the quotes
+discordWebhook = "https://discordapp.com/api/webhooks/629069286731677716/Bw9WY7s0_EQonKNC5H8N2FI97grvLnZxHzDJtsZ5gg_2WunUBpg5imWLHOHpOwv35JdF" #put your webhook in the quotes
 
-def webhook(discordWebhook, instagramUrl, newValue, changeType):
+def webhook(discordWebhook, instagramUrl, newValue, changeType, instagramUsername):
     hook = Webhook(discordWebhook)
     embed = Embed(
-    description="{} Change".format(changeType),
+    description=f"{changeType} Change - @{instagramUsername}",
     color=0XFFA574,
     timestamp='now',
     )
-    embed.add_field(name='Link', value=instagramUrl, inline=True) 
+    embed.add_field(name='Profile Link', value=instagramUrl, inline=True) 
     embed.add_field(name='New Update', value=newValue, inline=False) 
     embed.set_author(name='Instagran Monitor')
     embed.set_footer(text='@suprattle')
@@ -29,15 +29,30 @@ def webhook(discordWebhook, instagramUrl, newValue, changeType):
     except:
         hook.send(embed=embed)
 
+def webhook_post(discordWebhook, instagramUrl, postCaption, postImage, instagramUsername):
+    hook = Webhook(discordWebhook)
+    embed = Embed(
+    description="New Post ",
+    color=0XFFA574,
+    timestamp='now',
+    )
+    embed.add_field(name='Profile Link', value=str(instagramUrl), inline=True) 
+    embed.add_field(name='Post Caption', value=str(postCaption), inline=False) 
+    embed.set_author(name='Instagran Monitor')
+    embed.set_footer(text='@suprattle')
+    embed.set_image(str(postImage))
+    hook.send(embed=embed)
+
 
 # task code
 
 while True:
+    session = requests.session()
     instagramUrl = "https://www.instagram.com/" + instagramUsername
     headers = {
         'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
     }
-    r = requests.get(instagramUrl, headers=headers)
+    r = session.get(instagramUrl, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
     scriptJsonSrc = soup.find_all("script")
     scriptJsonSrc = scriptJsonSrc[4]
@@ -57,40 +72,46 @@ while True:
     instagramFollowers = scriptJson['entry_data']['ProfilePage'][0]['graphql']['user']['edge_followed_by']['count']
     instagramFollowing = scriptJson['entry_data']['ProfilePage'][0]['graphql']['user']['edge_follow']['count']
     instagramProfileImage = scriptJson['entry_data']['ProfilePage'][0]['graphql']['user']['profile_pic_url_hd']
+    userId = instagramProfileImage = scriptJson['entry_data']['ProfilePage'][0]['graphql']['user']["id"]
+    recentPostSrc = scriptJson['entry_data']['ProfilePage'][0]['graphql']['user']["edge_owner_to_timeline_media"]["edges"][0]["node"]["thumbnail_src"]
 
     # now we check these values against the previous checks values, it should throw an error the first time
 
     try:
+        if recentPostSrc != oldRecentPostSrc:
+            recentPostCaption = scriptJson['entry_data']['ProfilePage'][0]['graphql']['user']["edge_owner_to_timeline_media"]["edges"][0]["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
+            webhook_post(discordWebhook, instagramUrl, recentPostCaption, recentPostSrc, instagramUsername)
         if instagramBio != oldInstagramBio:
             print("Detected Change in Bio")
             print("Sending Webhook")
             changeType = "Bio Text"
             newValue = instagramBio
-            webhook(discordWebhook, instagramUrl, newValue, changeType)
+            webhook(discordWebhook, instagramUrl, newValue, changeType, instagramUsername)
         if instagramBioUrl != oldInstagramBioUrl:
             print("Detected Change in Bio Url")
             print("Sending Webhook")
             changeType = "Bio Url"
             newValue = instagramBioUrl
-            webhook(discordWebhook, instagramUrl, newValue, changeType)
+            webhook(discordWebhook, instagramUrl, newValue, changeType, instagramUsername)
         if instagramName != oldInstagramName:
             print("Detected Change in Name")
             print("Sending Webhook")
             changeType = "Name"
             newValue = instagramName
-            webhook(discordWebhook, instagramUrl, newValue, changeType)
+            webhook(discordWebhook, instagramUrl, newValue, changeType, instagramUsername)
         if int(instagramFollowing) > int(oldInstagramFollowing):
             print("Detected Change in Following")
             print("Sending Webhook")
             changeType = "Following"
             newValue = str(instagramFollowing)
-            webhook(discordWebhook, instagramUrl, newValue, changeType)
+            webhook(discordWebhook, instagramUrl, newValue, changeType, instagramUsername)
         oldInstagramBio = instagramBio
         oldInstagramBioUrl = instagramBioUrl
         oldInstagramName = instagramName
         oldInstagramFollowers = instagramFollowers
         oldInstagramFollowing = instagramFollowing 
         oldInstagramProfileImage = instagramProfileImage
+        oldRecentPostSrc = recentPostSrc
         time.sleep(int(retryDelay))
     except Exception as e:
         oldInstagramBio = instagramBio
@@ -99,6 +120,7 @@ while True:
         oldInstagramFollowers = instagramFollowers
         oldInstagramFollowing = instagramFollowing 
         oldInstagramProfileImage = instagramProfileImage
+        oldRecentPostSrc = recentPostSrc
         print("Hit Error - " + str(e))
         try:
             time.sleep(int(retryDelay))
